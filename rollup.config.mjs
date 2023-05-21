1da3f254ae2117d1dd574ac2a82c2e89e11c635c
John-Dag Addy <jaedagy@gmail.com>
2023-05-21 16:42:43 +0000

    fix: edit rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,6 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import terser from '@rollup/plugin-terser'
 import dts from 'rollup-plugin-dts'
-import postcss from 'rollup-plugin-postcss'
 
 import packageJson from './package.json' assert { type: 'json' }
 
@@ -29,7 +28,6 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
-      postcss(),
       terser(),
     ],
   },
@@ -37,6 +35,5 @@ export default [
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
-    external: [/\.css$/],
   },
 ]
