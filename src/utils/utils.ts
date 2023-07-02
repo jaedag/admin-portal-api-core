b555217f0dc280bf24061d2f0770022e4855657f
John-Dag Addy <jaedagy@gmail.com>
2023-07-02 14:22:36 +0000

    fix: specify type fo checkIfArrayHasRepeatingValues

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-export const checkIfArrayHasRepeatingValues = (array: any[]) => {
+export const checkIfArrayHasRepeatingValues = (array: (string | number)[]) => {
   const sortedArray = array.sort()
   for (let i = 0; i < sortedArray.length - 1; i += 1) {
     if (sortedArray[i + 1] === sortedArray[i]) {
