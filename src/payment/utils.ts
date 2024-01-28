3502b8fe6cfa58947e90b1d9abb208a8a5d475a9
John-Dag Addy <jaedagy@gmail.com>
2024-01-28 04:45:31 +0000

    refactor: payment utils and add validation for card details

--- src/payment/utils.ts
+++ src/payment/utils.ts
@@ -13,10 +13,8 @@ export const getMobileCode = (network: Network): NetworkCode => {
     case 'Tigo':
       return 'tgo'
     default:
-      break
+      return network
   }
-
-  return 'mtn'
 }
 
 export const addPaystackCharge = (amount: number): number =>
@@ -56,41 +54,60 @@ export const updatePaystackCustomerBody = ({
   },
 })
 
-export const initiatePaystackTransaction = ({
+export const initiatePaystackCharge = ({
   auth,
   amount,
   customer,
-  mobileNumber,
-  mobileNetwork,
+  mobile_money,
+  card,
   subaccount,
   customFields,
 }: {
   auth: string
   amount: number
   customer: Member
-  mobileNumber: string
-  mobileNetwork: Network
+  mobile_money?: {
+    phone: string
+    provider: Network
+  }
+  card?: {
+    number: string
+    cvv: number
+    expiry_month: number
+    expiry_year: number
+  }
   subaccount: string
   customFields?: { [key: string]: string }
-}) => ({
-  method: 'post',
-  baseURL: 'https://api.paystack.co/',
-  url: `/charge`,
-  headers: {
-    'content-type': 'application/json',
-    Authorization: auth,
-  },
-  data: {
-    amount: addPaystackCharge(amount),
-    email: customer.email,
-    currency: 'GHS',
-    subaccount,
-    mobile_money: {
-      phone: mobileNumber,
-      provider: getMobileCode(mobileNetwork),
+}) => {
+  if (card?.cvv.toString().length !== 3) {
+    throw new Error('cvv must be 3 digits long')
+  }
+
+  if (card?.expiry_month < 1 || card?.expiry_month > 12) {
+    throw new Error('expiry_month must be between 1 and 12')
+  }
+
+  return {
+    method: 'post',
+    baseURL: 'https://api.paystack.co/',
+    url: `/charge`,
+    headers: {
+      'content-type': 'application/json',
+      Authorization: auth,
     },
-    metadata: {
-      custom_fields: [customFields],
+    data: {
+      amount: addPaystackCharge(amount),
+      email: customer.email,
+      currency: 'GHS',
+      subaccount,
+      mobile_money: {
+        phone: mobile_money?.phone,
+        provider: getMobileCode(mobile_money?.provider || 'MTN'),
+      },
+      card,
+      metadata: {
+        custom_fields: [customFields],
+      },
     },
-  },
-})
+  }
+}
